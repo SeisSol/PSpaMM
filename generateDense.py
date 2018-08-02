24acddce419ae1d265e63278681412a6537d2ccf
Peter Wauligmann <peter.wauligmann@gmail.com>
2018-08-02 11:32:52 +0200

    ldb support

--- generateDense.py
+++ generateDense.py
@@ -5,10 +5,11 @@ import sys
 
 m=int(sys.argv[1])
 n=int(sys.argv[2])
+ld=int(sys.argv[3])
 
 print('%%MatrixMarket matrix coordinate integer general')
 print('%')
-print('{} {} {}'.format(m,n,n*m))
+print('{} {} {}'.format(ld,n,n*m))
 for col in range(n):
   for row in range(m):
     print('{} {} {}'.format(row+1, col+1, 1))
