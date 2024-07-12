b8030a429c4ecaa2d1adeac3d2b96246fe23ea1e
David Schneller <david.schneller@tum.de>
2024-07-12 16:06:41 +0200

    Fix testsuite results

--- tests/testsuite_generator.py
+++ tests/testsuite_generator.py
@@ -181,7 +181,7 @@ end_of_testsuite = """
 
   printf("\\n%i out of %lu test successful!\\n", correct, results.size());
 
-  return 0 ? correct == results.size() : 1;
+  return correct == results.size() ? 0 : 1;
 }
 """
 
