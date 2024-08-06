3f67d54b9d2114169785cefd3b28eb34af9a7b17
David Schneller <david.schneller@tum.de>
2024-08-06 19:53:07 +0200

    Rollback condition extension

--- pspamm/scripts/max_hsw.py
+++ pspamm/scripts/max_hsw.py
@@ -6,23 +6,24 @@ def getBlocksize(m , n, bk, v_size=4):
 
 	for i in range(v_size, m+1, v_size):
 		for j in range(1, n+1):
+			# can be replaced by HSW_condition_extended here
 			if HSW_condition(i, j, bk, v_size):
-				if i*j > maxval and (HSW_condition_safe(i, j, bk, v_size) or j > 1):
+				if i*j > maxval and (HSW_condition(i, j, bk, v_size) or j > 1):
 					maxval = i*j
 					bm = i
 					bn = j 
 
-	while HSW_condition_safe(bm, bn, bk+1, v_size):
+	while HSW_condition(bm, bn, bk+1, v_size):
 		bk += 1
 
 	return (bm, bn, bk)
 
-def HSW_condition_safe(bm, bn, bk, v_size):
+def HSW_condition(bm, bn, bk, v_size):
 	# ceiling division
 	vm = -(bm // -v_size)
 	return (bn + bk) * vm + bn * bk <= 16
 
-def HSW_condition(bm, bn, bk, v_size):
+def HSW_condition_extended(bm, bn, bk, v_size):
 	# ceiling division
 	vm = -(bm // -v_size)
 	return bn * vm + bn * bk + 1 <= 16
